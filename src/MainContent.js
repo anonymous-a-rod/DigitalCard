8b0514c8fb5b2ac87424729c46a249e3bcb23219
Aaron <aaron1venema@gmail.com>
2023-02-20 12:39:45 -0600

    bio

--- src/MainContent.js
+++ src/MainContent.js
@@ -2,7 +2,7 @@ const MainContent = () => {
     return ( 
         <div className="main">
             <h3>About</h3>
-            <p>As a creative individual, I am dedicated to my work as a front-end developer. My hardworking nature allows me to excel in designing and implementing user-friendly websites and applications. My passion lies in creating visually appealing and interactive experiences for users.</p>
+            <p>As a hardworking individual, I am dedicated to my work as a front-end developer. My creative nature allows me to excel in designing and implementing user-friendly websites and applications. My passion lies in creating visually appealing and interactive experiences for users.</p>
             <br />
             <h3>Interests</h3>
             <p>In my free time, I enjoy playing games with friends and family. I also like to stay active by going for bike rides and exploring new trails. Additionally, I am interested in language learning and enjoy finding new ways to improve my language skills.</p>
